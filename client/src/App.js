71a327880f163d2bbc0f52de5a53ae60f2fb8a7f
Engin <hibernationtheory@gmail.com>
2017-05-29 16:19:09 -0400

    simplfy the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,19 @@
 import React, { Component } from 'react';
-import SelectedFoods from './SelectedFoods';
-import FoodSearch from './FoodSearch';
+import Client from './Client';
 
 class App extends Component {
-  state = {
-    selectedFoods: [],
-  }
-
-  removeFoodItem = (itemIndex) => {
-    const filteredFoods = this.state.selectedFoods.filter(
-      (item, idx) => itemIndex !== idx,
-    );
-    this.setState({ selectedFoods: filteredFoods });
-  }
-
-  addFood = (food) => {
-    const newFoods = this.state.selectedFoods.concat(food);
-    this.setState({ selectedFoods: newFoods });
+  onClick() {
+    Client.search('hello')
+      .then((result) => {
+        console.log(result);
+      });
   }
 
   render() {
-    const { selectedFoods } = this.state;
-
     return (
-      <div className='App'>
-        <div className='ui text container'>
-          <SelectedFoods
-            foods={selectedFoods}
-            onFoodClick={this.removeFoodItem}
-          />
-          <FoodSearch
-            onFoodClick={this.addFood}
-          />
-        </div>
+      <div>
+        Hello World
+        <button onClick={this.onClick.bind(this)}>Test API</button>
       </div>
     );
   }
--- server.js
+++ server.js
@@ -1,10 +1,4 @@
 const express = require('express');
-const fs = require('fs');
-const sqlite = require('sql.js');
-
-const filebuffer = fs.readFileSync('db/usda-nnd.sqlite3');
-
-const db = new sqlite.Database(filebuffer);
 
 const app = express();
 
@@ -15,16 +9,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
-const COLUMNS = [
-  'carbohydrate_g',
-  'protein_g',
-  'fa_sat_g',
-  'fa_mono_g',
-  'fa_poly_g',
-  'kcal',
-  'description',
-];
-app.get('/api/food', (req, res) => {
+app.get('/api/test', (req, res) => {
   const param = req.query.q;
 
   if (!param) {
@@ -32,36 +17,10 @@ app.get('/api/food', (req, res) => {
       error: 'Missing required parameter `q`',
     });
     return;
-  }
-
-  // WARNING: Not for production use! The following statement
-  // is not protected against SQL injections.
-  const r = db.exec(`
-    select ${COLUMNS.join(', ')} from entries
-    where description like '%${param}%'
-    limit 100
-  `);
-
-  if (r[0]) {
-    res.json(
-      r[0].values.map((entry) => {
-        const e = {};
-        COLUMNS.forEach((c, idx) => {
-          // combine fat columns
-          if (c.match(/^fa_/)) {
-            e.fat_g = e.fat_g || 0.0;
-            e.fat_g = (
-              parseFloat(e.fat_g, 10) + parseFloat(entry[idx], 10)
-            ).toFixed(2);
-          } else {
-            e[c] = entry[idx];
-          }
-        });
-        return e;
-      }),
-    );
   } else {
-    res.json([]);
+    res.json({
+      'data': 'Hello World!'
+    });
   }
 });
 
